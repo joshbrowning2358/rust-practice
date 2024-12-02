ba7422f35918cde8a1fc36288ccfd085baf9a5c1
Josh Browning <joshua.browning@upstart.com>
2024-12-02 05:26:10 -0700

    finished puzzle1

--- advent-of-code-2024/src/bin/puzzle1.rs
+++ advent-of-code-2024/src/bin/puzzle1.rs
@@ -1,3 +1,5 @@
+use num::abs;
+
 mod file_reader;
 
 fn main() {
@@ -14,9 +16,44 @@ fn main() {
 }
 
 fn part_a(file_path: &str) -> i32 {
-    return 0
+    let (mut left, mut right) = parse_input(&file_path);
+    left.sort();
+    right.sort();
+
+    let mut total = 0;
+    for (left_val, right_val) in left.iter().zip(right.iter_mut()) {
+        total += abs(*left_val - *right_val);
+    }
+    return total
 }
 
 fn part_b(file_path: &str) -> i32 {
-    return 0
+    let (mut left, mut right) = parse_input(&file_path);
+    left.sort();
+    right.sort();
+
+    let mut total = 0;
+    for l in left.iter() {
+        // let cnt = l * (right.iter().filter(|&n| *n == *l).count() as i32);
+        // println!("Val is {l} and count is {cnt}");
+        total += l * (right.iter().filter(|&n| *n == *l).count() as i32);
+    }
+    return total
+}
+
+fn parse_input(file_path: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    if let Ok(lines) = file_reader::read_lines(file_path) {
+        for line in lines {
+            if let Ok(pipes) = line {
+                let (left_val, right_val) = pipes.split_once("   ").unwrap();
+                left.push(left_val.parse::<i32>().unwrap());
+                right.push(right_val.parse::<i32>().unwrap());
+            }
+        }
+    }
+
+    return (left, right)
 }
