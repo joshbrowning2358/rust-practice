9a5564c76dd5f0741107076d0db8e6cf2d33dfbb
Josh Browning <joshua.browning@upstart.com>
2024-12-02 10:05:53 -0700

    puzzle2

--- advent-of-code-2024/src/bin/puzzle2.rs
+++ advent-of-code-2024/src/bin/puzzle2.rs
@@ -1,3 +1,5 @@
+use num::abs;
+
 mod file_reader;
 
 fn main() {
@@ -14,22 +16,75 @@ fn main() {
 }
 
 fn part_a(file_path: &str) -> i32 {
-    return 0
-}
-
-fn part_b(file_path: &str) -> i32 {
-    return 0
-}
-
-fn parse_input(file_path: &str) -> Vec<Vec<i32>> {
-    let mut result = Vec::new();
-
+    let mut result: i32 = 0;
+    let mut vals: Vec<i32>;
+    let mut failed;
     if let Ok(lines) = file_reader::read_lines(file_path) {
         for line in lines {
-            if let Ok(pipes) = line {
+            if let Ok(report) = line {
+                vals = report.split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
+                failed = is_unsafe(vals);
+                if !failed {
+                    result += 1;
+                }
             }
         }
     }
+    return result
+}
 
-    return result;
+fn part_b(file_path: &str) -> i32 {
+    let mut result: i32 = 0;
+    let mut vals: Vec<i32>;
+    let mut failed: bool;
+    let mut any_passed: bool = false;
+    if let Ok(lines) = file_reader::read_lines(file_path) {
+        for line in lines {
+            if let Ok(report) = line {
+                vals = report.split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
+                let mut i: usize = 0;
+                while !any_passed && i < vals.len() {
+                    let new_vals = [&vals[0..i], &vals[(i + 1)..]].concat();
+                    failed = is_unsafe(new_vals);
+                    if !failed {
+                        any_passed = true;
+                    }
+                    i += 1;
+                }
+                if any_passed {
+                    result += 1;
+                }
+                any_passed = false;
+            }
+        }
+    }
+    return result
+}
+
+fn is_unsafe(report: Vec<i32>) -> bool {
+    let mut inc_cnt = 0;
+    let mut failed = false;
+    for diffs in report.windows(2) {
+        if diffs[0] < diffs[1] {
+            if inc_cnt < 0 {
+                failed = true;
+                break
+            } else {
+                inc_cnt += 1;
+            }
+        }
+        if diffs[0] > diffs[1] {
+            if inc_cnt > 0 {
+                failed = true;
+                break
+            } else {
+                inc_cnt -= 1;
+            }
+        }
+        if abs(diffs[1] - diffs[0]) > 3 || abs(diffs[1] - diffs[0]) < 1 {
+            failed = true;
+            break
+        }
+    } 
+    return failed
 }
