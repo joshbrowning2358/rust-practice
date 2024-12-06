49185b658c9236d1f8ad62173ae461ddc2b3e778
Josh Browning <joshua.browning@upstart.com>
2024-12-06 04:42:22 -0700

    puzzle 2021 4

--- advent-of-code-2021/src/bin/puzzle4.rs
+++ advent-of-code-2021/src/bin/puzzle4.rs
@@ -1,8 +1,4 @@
-use nalgebra::{SMatrix, matrix};
-//use nalgebra::base::dimension::U25;
 mod file_reader;
-
-//type Matrix5x5 = MatrixN<i32, U25>;
 
 fn main() {
     let full_path = file!();
@@ -17,60 +13,118 @@ fn main() {
     println!("Answer to {file_name} b is {ans};");
 }
 
-fn parse_input(file_path: &str) -> (Vec<i32>, Vec<SMatrix<i32, 5, 5>>) {
-//fn parse_input(file_path: &str) -> Vec<i32> {
-    let mut called_nums: Vec::<i32> = Vec::<i32>::new();
-    let mut boards: Vec::<SMatrix<i32, 5, 5>> = Vec::<SMatrix<i32, 5, 5>>::new();
+fn parse_input(file_path: &str) -> (Vec<u16>, Vec<Vec<Vec<u16>>>) {
+    let mut called_nums: Vec::<u16> = Vec::<u16>::new();
+    let mut boards = Vec::new();
 
-    let mut board: SMatrix<i32, 5, 5> = matrix![0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0];
+    let mut board: Vec<Vec<u16>> = Vec::new();
     let mut board_idx: usize = 0;
-    let mut row: Vec::<i32>;
+    let mut row: Vec::<u16>;
 
     if let Ok(mut lines) = file_reader::read_lines(file_path) {
         let line = lines.next().unwrap().unwrap();
-        called_nums = line.split(',').map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        called_nums = line.split(',').map(|x| x.parse::<u16>().unwrap()).collect::<Vec<u16>>();
 
         for line in lines {
             if let Ok(board_line) = line {
-                println!("Line is {board_line:?}");
                 if board_idx > 0 {
                     row = board_line
                       .split(' ')
                       .filter(|s| !s.is_empty())  // Handle consecutive spaces in parsing
-                      .map(|x| x.parse::<i32>().unwrap())
-                      .collect::<Vec<i32>>();
-                    for col in 0..5 {
-                        board[(board_idx - 1, col)] = row[col];
-                    }
+                      .map(|x| x.parse::<u16>().unwrap())
+                      .collect::<Vec<u16>>();
+                    board.push(row);
                 }
                 if board_idx == 5 {
                     boards.push(board);
                     board_idx = 0;
+                    board = Vec::new();
                 } else {
                     board_idx += 1;
                 }
             }
         }
     }
-    println!("Found called_nums {called_nums:?} and boards {boards:?}");
+    // println!("Called nums is {called_nums:?}");
     return (called_nums, boards)
 }
 
 fn part_a(file_path: &str) -> i32 {
     let (called_nums, boards) = parse_input(file_path);
+    let mut fewest_nums = 100;
+    let mut score = 0;
+
     for board in boards {
-        let filled: SMatrix<i32, 5, 5> = matrix![0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0; 0, 0, 0, 0, 0];
-        for called_num in called_nums {
-            if let Ok([idx, idx2]) = board.iter().position(|&r| r == called_num) {
-                // Only operate if row,col is found
-                println!("Found idx {idx}");
-                // filled[(row, col)] = 1;
+        // println!("Trying board {board:?}");
+        let mut row_counts = [0, 0, 0, 0, 0];
+        let mut col_counts = [0, 0, 0, 0, 0];
+        let mut curr_num = 0;
+        let is_winner = false;
+        let mut uncalled_vals: Vec<u16> = board.clone().into_iter().flatten().collect();
+
+        while !is_winner {
+            for i in 0..5 {
+                for j in 0..5 {
+                    if board[i][j] == called_nums[curr_num] {
+                        row_counts[i] += 1;
+                        col_counts[j] += 1;
+                        uncalled_vals.retain(|value| *value != called_nums[curr_num]);
+                    }
+                }
             }
+            if *row_counts.iter().max().unwrap() == 5 || *col_counts.iter().max().unwrap() == 5 {
+                break
+            }
+            curr_num += 1;
+            let val = called_nums[curr_num];
+            // println!("Checking {val}, Row counts are {row_counts:?} and col_counts {col_counts:?}");
+        }
+        if curr_num < fewest_nums {
+            fewest_nums = curr_num;
+            // compute score
+            score = (uncalled_vals.iter().sum::<u16>() as i32) * (called_nums[curr_num] as i32);
+            println!("Found better board with nums={fewest_nums} and score {score} and curr_num {curr_num} and uncalled_vals {uncalled_vals:?}");
         }
     }
-    return 0
+    return score
 }
 
 fn part_b(file_path: &str) -> i32 {
-    return 0
+    let (called_nums, boards) = parse_input(file_path);
+    let mut most_nums = 0;
+    let mut score = 0;
+
+    for board in boards {
+        // println!("Trying board {board:?}");
+        let mut row_counts = [0, 0, 0, 0, 0];
+        let mut col_counts = [0, 0, 0, 0, 0];
+        let mut curr_num = 0;
+        let is_winner = false;
+        let mut uncalled_vals: Vec<u16> = board.clone().into_iter().flatten().collect();
+
+        while !is_winner {
+            for i in 0..5 {
+                for j in 0..5 {
+                    if board[i][j] == called_nums[curr_num] {
+                        row_counts[i] += 1;
+                        col_counts[j] += 1;
+                        uncalled_vals.retain(|value| *value != called_nums[curr_num]);
+                    }
+                }
+            }
+            if *row_counts.iter().max().unwrap() == 5 || *col_counts.iter().max().unwrap() == 5 {
+                break
+            }
+            curr_num += 1;
+            let val = called_nums[curr_num];
+            // println!("Checking {val}, Row counts are {row_counts:?} and col_counts {col_counts:?}");
+        }
+        if curr_num > most_nums {
+            most_nums = curr_num;
+            // compute score
+            score = (uncalled_vals.iter().sum::<u16>() as i32) * (called_nums[curr_num] as i32);
+            println!("Found worse board with nums={most_nums} and score {score} and curr_num {curr_num} and uncalled_vals {uncalled_vals:?}");
+        }
+    }
+    return score
 }
