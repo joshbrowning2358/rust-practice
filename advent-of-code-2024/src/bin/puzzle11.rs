71a8d804ab860e25025898b258d05ea7fb2e0b70
Josh Browning <joshua.browning@upstart.com>
2024-12-12 05:16:25 -0700

    puzzle 11 2024 with memoization

--- advent-of-code-2024/src/bin/puzzle11.rs
+++ advent-of-code-2024/src/bin/puzzle11.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use memoize::memoize;
 
 mod file_reader;
 
@@ -34,11 +35,9 @@ fn part_b(file_path: &str) -> i64 {
     let stones = parse_input(file_path);
     let n_splits = 75;
 
-    let mut cache: HashMap<(u64, u8), i64> = HashMap::new();
-
     let mut result: i64 = 0;
     for stone in stones {
-        let cnt = split_stone(stone, n_splits, &mut cache);
+        let cnt = split_stone_mem(stone, n_splits);
         result += cnt;
         // println!("Cache is {cache:?}");
     }
@@ -90,5 +89,35 @@ fn split_stone(stone: u64, n_splits: u8, cache: &mut HashMap<(u64, u8), i64>) ->
         }
     }
     cache.insert((stone, n_splits), ans);
+    return ans
+}
+
+#[memoize]
+fn split_stone_mem(stone: u64, n_splits: u8) -> i64 {
+    let mut log: u32 = 0;
+    if stone > 0 {
+        log = stone.ilog10();
+    }
+    let ans;
+    if n_splits == 1 {
+        if stone == 0 {
+            ans = 1;
+        } else if log % 2 == 1 {
+            ans = 2;
+        } else {
+            ans = 1;
+        }
+    } else {
+        if stone == 0 {
+            ans = split_stone_mem(1, n_splits - 1);
+        } else if log % 2 == 1 {
+            let large = stone / 10u64.pow((log + 1) / 2);
+            let small = stone - large * 10u64.pow((log + 1) / 2);
+            // println!("Splitting {stone} into {large} and {small}");
+            ans = split_stone_mem(large, n_splits - 1) + split_stone_mem(small, n_splits - 1);
+        } else {
+            ans = split_stone_mem(stone * 2024, n_splits - 1);
+        }
+    }
     return ans
 }
