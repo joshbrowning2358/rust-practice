3740fa7924653ee66058dc6c2ff239be2f7d4158
Josh Browning <joshua.browning@upstart.com>
2025-01-13 17:25:19 -0700

    implement dijkstra

--- advent_of_code_2022/src/common.rs
+++ advent_of_code_2022/src/common.rs
@@ -1,6 +1,12 @@
 // An attribute to hide warnings for unused code.
 #![allow(dead_code)]
 
+use std::cmp::PartialEq;
+use std::collections::HashSet;
+use std::hash::Hash;
+
+use binary_heap_plus::BinaryHeap;
+
 #[derive(Debug, Clone, Eq, Hash, Copy)]
 pub struct Point<T> {
     pub x: T,
@@ -35,6 +41,58 @@ pub fn display_grid(grid: Vec<Vec<char>>) {
 //    }
 //}
 
-pub fn dijkstra(start<T>, end<T>) {
-    println!("Test");
+#[derive(Clone, Eq, Hash)]
+pub struct Node<T: Eq + Hash + Clone, V: std::cmp::Ord + Hash + Clone> {
+    pub pt: Point<T>,
+    pub path: Vec<Node<T, V>>,
+    pub dist: V,
+}
+
+impl<T: Eq + Hash + Clone, V: std::cmp::Ord + Hash + Clone> PartialEq for Node<T, V> {
+    fn eq(&self, other: &Self) -> bool {
+        (self.pt.x == other.pt.x) & (self.pt.y == other.pt.y)
+    }
+}
+
+pub fn dijkstra<T: Eq + Hash + Clone, V: std::cmp::Ord + Hash + Clone>(start: Node<T, V>, end: Node<T, V>, get_neighbors: impl Fn(&Node<T, V>) -> Vec<Node<T, V>>) -> (V, Vec<Node<T, V>>) {
+    // Types:
+    //    T: Type of x/y points (probably i32, u32, f32, etc.)
+    //    V: Type of dist (probably u32 or f32)
+    // Args:
+    //    start: Starting node
+    //    end: Ending node
+    //    get_neighbors: Function getting neighbors when given node.  Should also update the paths of said neighbors to include themselves.
+    let mut unvisited = BinaryHeap::from_vec_cmp(
+        vec![], |a: &Node<T, V>, b: &Node<T, V>| b.dist.cmp(&a.dist)
+    );
+    let mut visited: HashSet::<Node<T, V>> = HashSet::new();
+    // let mut to_explore_dists: HashMap::<Point, i32> = HashMap::new();
+
+    unvisited.push(start);
+
+    let final_dist: V;
+    let final_path: Vec<Node<T, V>>;
+    loop {
+        let curr_node = unvisited.pop().unwrap();
+        if visited.contains(&curr_node) {
+            continue
+        }
+        visited.insert(curr_node.clone());
+        if curr_node == end {
+            final_dist = curr_node.dist;
+            final_path = curr_node.path;
+            break
+        }
+        let candidates = get_neighbors(&curr_node);
+        for cand in candidates {
+            if visited.contains(&cand) {
+                continue
+            }
+            unvisited.push(cand);
+        }
+        if unvisited.len() == 0 {
+            panic!("No new nodes to visit!");
+        }
+    }
+    return (final_dist, final_path)
 }
--- advent_of_code_2022/src/puzzles/puzzle12.rs
+++ advent_of_code_2022/src/puzzles/puzzle12.rs
@@ -1,14 +1,28 @@
+use crate::common::{Point, dijkstra, Node};
 use crate::file_reader;
 use crate::puzzle_solver;
 
-pub struct Puzzle
+pub struct Puzzle12;
 
-impl puzzle_solver::Solver for Puzzle {
+impl puzzle_solver::Solver for Puzzle12 {
     fn part_1(&self, file_path: &str) -> String {
+        let (grid, start, end) = parse(file_path);
+        println!("Grid is {grid:?}, start {start:?}, end {end:?}");
+
+        fn get_neighbors(n: &Node<i16, i16>) -> Vec<Node<i16, i16>> {
+            return vec![]
+        }
+        dijkstra::<i16, i16>(
+            Node{pt: start, dist: 0, path: vec![]},
+            Node{pt: end, dist: 10000, path: vec![]},
+            get_neighbors 
+        );
         return String::from("TODO")
     }
 
     fn part_2(&self, file_path: &str) -> String {
+        let (grid, start, end) = parse(file_path);
+        println!("Grid is {grid:?}, start {start:?}, end {end:?}");
         return String::from("TODO")
     }
 
@@ -17,16 +31,33 @@ impl puzzle_solver::Solver for Puzzle {
     }
 
     fn name(&self) -> &'static str {
-        "Day : "
+        "Day 12: Hill Climbing Algorithm"
     }
 }
 
-fn parse(file_path: &str) -> {
+fn parse(file_path: &str) -> (Vec<Vec<u8>>, Point<i16>, Point<i16>) {
+    let mut grid: Vec<Vec<u8>> = vec![];
+    let mut start: Point<i16> = Point{x: 0, y: 0};
+    let mut end: Point<i16> = Point{x: 0, y: 0};
+
     if let Ok(lines) = file_reader::read_lines(file_path) {
-        for line in lines {
+        for (row_idx, line) in lines.enumerate() {
             if let Ok(row) = line {
-                ... = row.
+                let mut grid_row: Vec<u8> = vec![];
+                for (col_idx, alpha_height) in row.chars().enumerate() {
+                    let mut height = alpha_height.to_ascii_lowercase() as u8 - 96;
+                    if alpha_height == 'S' {
+                        height = 1;
+                        start = Point{x: col_idx as i16, y: row_idx as i16};
+                    } else if alpha_height == 'E' {
+                        height = 26;
+                        end = Point{x: col_idx as i16, y: row_idx as i16};
+                    }
+                    grid_row.push(height);
+                }
+                grid.push(grid_row);
+            }
         }
     }
-    return
+    return (grid, start, end)
 }
